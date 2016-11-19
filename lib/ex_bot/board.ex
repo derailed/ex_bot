f2bbb8f80b18d727e668372e90d1a512ecc163ff
derailed <fernand.galiana@gmail.com>
2016-11-19 11:51:39 -0700

    updates

--- lib/ex_bot/board.ex
+++ lib/ex_bot/board.ex
@@ -3,24 +3,9 @@ defmodule ExBot.Board do
   Virtual representation of an IOT installation
   """
 
-  # BOZO !! Don't pass the socket here. Let the board
-  # tell you about event. All add API to add pins and bus
-
-  # @enforce_keys [:gpios, :i2c, :socket, :board]
-  defstruct [:gpios, :i2c, :socket, :board]
+  defstruct [:gpios, :i2c, :i2c_bus, :socket, :board]
 
   alias ExBot.{I2CBus, Gpio}
-
-  @buzz_pin   17
-  @laser_pin  23
-  @merc_pin    5
-
-  @photo_chan 0
-  @temp_chan  1
-  @mic_chan   2
-
-  # BOZO !! Have socket monitor board and trigger event
-  # BOZO !! Channel should be the controller for the board!
 
   def buzz() do
     {:ok, buzz} = Gpio.init(@buzz_pin, :output)
@@ -32,95 +17,59 @@ defmodule ExBot.Board do
     IO.inspect Application.fetch_env(:ex_bot, :i2c)
     IO.inspect Application.fetch_env(:ex_bot, :fred)
 
-    {:ok, bus} = case Application.fetch_env(:ex_bot, :i2c) do
-      {:ok, conf} -> I2CBus.init(conf[:channel], conf[:address])
-      :error      -> {:ok, nil}
+    bus_config = Application.fetch_env(:ex_bot, :i2c)
+    {i2c, i2c_devs} = case bus_config do
+      {:ok, c} -> { I2CBus.init(c[:channel], c[:address]), c[:bus] }
+      error    -> { nil, [] }
     end
-    IO.inspect bus
-    # {:ok, bus} = I2CBut.init(chan, address)
-    # board.i2c = bus
+    IO.puts "Ic2s"
+    IO.inspect i2c
+    IO.inspect i2c_devs
 
-    # gpios = for p <- gpios, into: [] do
-    #    {:ok, gpio} = Gpio.init(p)
-    #    gpio
-    # end
-    # board.gpios = gpios
+    {:ok, gpios} = case Application.fetch_env(:ex_bot, :gpios) do
+      {:ok, devices} ->
+        { :ok,
+          for d <- gpios_devs, do
+            case Gpio.init(d[:pin], d[:direction]) do
+              {:ok, dev} -> [device: dev, config: d]
+              :error     -> raise "Unable to initialize pin `#{d[:pin]}"
+            end
+          end
+        }
+      :error -> {:ok, []}
+    end
+
     %ExBot.Board{
-      i2c:    bus,
-      board:  self
+      i2c:     i2c,
+      i2c_bus: i2c_devs,
+      gpios:   gpios,
+      board:   self
     }
   end
 
-  @doc """
-  Build software representation for installed hardware
-  """
-  def layout(socket) do
-    {:ok, buzz} = Gpio.init(@buzz_pin, :output)
-    buzz |> Gpio.toggle(:off)
-    # socket |> register(:buzz)
-
-    {:ok, laser} = Gpio.init(@laser_pin, :output)
-    laser |> Gpio.toggle(:on)
-    # socket |> register(:laser)
-
-    {:ok, merc} = Gpio.init(@merc_pin, :input)
-    # socket |> register(:merc)
-
-    {:ok, bus} = I2CBus.init("1", 0x48)
-    # socket |> register(:temp)
-    # socket |> register(:mic)
-    # socket |> register(:photo)
-
-    board = %ExBot.Board{
-      gpios:  %{laser: laser, buzz: buzz, merc: merc},
-      i2c:    bus,
-      socket: socket,
-      board:  self
-    }
-
-    IO.puts "Board Running..."
-    spawn(fn -> board |> run() end)
-
-    {:ok, board}
+  def toggle(board, name, state) do
+    board.locate(:gpio, name)
+    |> Gpio.toggle(state)
+    board
   end
 
-  # defp register(socket, sensor) do
-  #   Presence.track(socket, sensor, %{
-  #       device:    "browser",
-  #       sensor:    sensor,
-  #       online_at: inspect(:os.timestamp())
-  #   })
-  #   socket.channel.push socket, "presence_state", Presence.list(socket)
-  # end
-
-  defp send(socket, topic, val) do
-    socket.channel.handle_out(
-      Atom.to_string(topic),
-      %{val: val},
-      socket
-    )
+  def read(board, :i2c, name) do
+    {:ok, dev} = boad.locate(:i2c, name)
+    board.i2c |> I2CBus.read(dev)
+  end
+  def read(board, :gpio, name) do
+    {:ok, dev} = board |> locate(:gpio, name)
+    dev |> Gpio.read
   end
 
-  def run(board) do
-    val = board.i2c |> I2CBus.read(@photo_chan)
-    # IO.puts "Laser: #{val}"
-    board.socket |> send(:laser, val)
-
-    val = board.i2c |> I2CBus.read(@temp_chan)
-    # IO.puts "Temp: #{val}"
-    board.socket |> send(:temp, val)
-
-    val = board.i2c |> I2CBus.read(@mic_chan)
-    # IO.puts "Mic: #{val}"
-    board.socket |> send(:mic, val)
-
-    val = Gpio.read(board.gpios.merc)
-    # IO.puts "Merc: #{val}"
-    board.socket |> send(:merc, val)
-
-    board.gpios.buzz |> Gpio.toggle(:off)
-
-    :timer.sleep(1_000)
-    run(board)
+  defp locate(board, :gpio, name) do
+    board.gpios
+    |> Enum.filter(&(&1.config[:name] == name))
+    |> List.first
+  end
+  defp locate(board, :i2c, name) do
+    board.i2c_bus
+    |> Enum.filter(&(&1.config[:name] == name))
+    |> List.first
   end
 end
